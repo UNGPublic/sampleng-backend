cb0e2de6f984df63bdd1b8a7134fa6fa13ce1d3b
ungppublic <alemser@softplan.com.br>
2014-11-10 16:40:08 -0200

    Atualização do cors filter.

--- src/main/java/ungp/sampleng/backend/SampleApplication.java
+++ src/main/java/ungp/sampleng/backend/SampleApplication.java
@@ -1,6 +1,7 @@
 package ungp.sampleng.backend;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import ungp.sampleng.backend.filter.CORSResponseFilter;
 import ungp.sampleng.backend.repository.ProprietarioRepository;
 import ungp.sampleng.backend.resource.VeiculoResource;
 
@@ -12,6 +13,7 @@ public class SampleApplication extends ResourceConfig {
     public SampleApplication() {
         System.out.println("Iniciando ApplicationPath...");
         super.packages(true, "ungp.sampleng.backend.resource", "ungp.sampleng.backend.exception");
+        super.register(CORSResponseFilter.class);
     }
 
 }
---
+++ src/main/java/ungp/sampleng/backend/filter/CORSResponseFilter.java
@@ -0,0 +1,24 @@
+package ungp.sampleng.backend.filter;
+
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import java.io.IOException;
+
+public class CORSResponseFilter
+        implements ContainerResponseFilter {
+
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        headers.add("Access-Control-Allow-Credentials", "true");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        headers.add("Access-Control-Max-Age", "1209600");
+    }
+
+}
