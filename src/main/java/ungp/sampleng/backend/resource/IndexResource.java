4a2e351040789323a13ca275649315fb380acada
alemser <lemser.alessandro@gmail.com>
2014-11-13 09:14:36 -0200

    Ajuste do index.

--- src/main/java/ungp/sampleng/backend/resource/IndexResource.java
+++ src/main/java/ungp/sampleng/backend/resource/IndexResource.java
@@ -13,5 +13,6 @@ public class IndexResource {
         return "<A href='/sample/res/application.wadl'>WSLD</a><br/>" +
                 "JSON exemplo: {\"placa\":\"ABC0102\", \"renavam\": \"9854621222\", \"proprietario\":{\"nome\": \"John Java\", \"cnh\":\"0123456789\", \"cpf\":\"11122233344\"}}";
     }
+    
 
 }
