e9e22799fa11e3559c6640bad4f49f93a8d7778d
Rodrigo Silva <rcandidosilva@gmail.com>
2014-11-24 17:24:07 -0200

    Latests changes on CAS configuration

---
+++ src/main/java/ungp/sampleng/backend/util/ApplicationContextProvider.java
@@ -0,0 +1,19 @@
+package ungp.sampleng.backend.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class ApplicationContextProvider implements ApplicationContextAware {
+
+    private static ApplicationContext context;
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
